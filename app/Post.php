be71720a8ebb5ccd0c0b10767b826777ce62bf8f
Tillmann Weimer <tillmann1970@gmail.com>
2020-02-10 13:40:40 +0100

    created relations

--- app/Post.php
+++ app/Post.php
@@ -2,9 +2,21 @@
 
 namespace App;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+
+    /**
+     * The attributes that are mass assignable
+     */
+    protected $fillable = [
+        'user_id', 'title', 'content',
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+
+    }
 }
