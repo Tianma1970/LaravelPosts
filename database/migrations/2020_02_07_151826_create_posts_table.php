874a62b44e889efa18b4aa019f5f3fb5bc4039d0
Tillmann Weimer <tillmann1970@gmail.com>
2020-02-10 13:43:16 +0100

    added row

--- database/migrations/2020_02_07_151826_create_posts_table.php
+++ database/migrations/2020_02_07_151826_create_posts_table.php
@@ -15,6 +15,9 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->string('title');
+            $table->text('content');
             $table->timestamps();
         });
     }
