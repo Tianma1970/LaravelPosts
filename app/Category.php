008260a3c304012a5815123540aeb2c67e390d78
Tillmann Weimer <tillmann1970@gmail.com>
2020-02-11 13:38:05 +0100

    added relations

--- app/Category.php
+++ app/Category.php
@@ -2,9 +2,20 @@
 
 namespace App;
 
+use App\Post;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+    ];
+
+    public function categories() {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+
+    public function posts() {
+        return $this->hasMany(Post::class);
+    }
 }
