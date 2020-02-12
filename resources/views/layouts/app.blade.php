3b5f0ed94b0d59ef71d6c83d5e40747636940afe
Tillmann Weimer <tillmann1970@gmail.com>
2020-02-12 17:08:52 +0100

    added link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,6 +34,9 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item">
+                            <a class="nav-link" href="/Allposts">All Posts</a>
+                        </li>
+                        <li class="nav-item">
                             <a class="nav-link" href="/countries">Countries</a>
                         </li>
                         <li class="nav-item">
