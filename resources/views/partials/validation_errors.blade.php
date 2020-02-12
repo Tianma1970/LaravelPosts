169a9a54fd692f6c530353b457dbd051668b0744
Tillmann Weimer <tillmann1970@gmail.com>
2020-02-12 17:10:27 +0100

    added col class

--- resources/views/partials/validation_errors.blade.php
+++ resources/views/partials/validation_errors.blade.php
@@ -1,5 +1,5 @@
 @if($errors->any())
-    <div class="alert alert-warning">
+    <div class="alert alert-warning col-6">
         @foreach($errors->all() as $error)
             {{ $error }}
         @endforeach
