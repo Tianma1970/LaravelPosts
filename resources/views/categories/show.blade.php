c842f0b12145b1f2f4b9a0e3153ffa1315d5bdc6
Tillmann Weimer <tillmann1970@gmail.com>
2020-02-12 17:10:03 +0100

    added bootstrap card style

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -4,12 +4,27 @@
 
 <div class="container mt-3 col-6">
     <h1>Posts from Category {{ $category->name }} </h1>
-    @if(count($category->posts) > 0)
-        @foreach($category->posts as $post)
-            <h3>{{ $post->title }}</h3>
-            <p>{{ $post->content }}</p>
-        @endforeach
-    @endif
+            @if(count($category->posts) > 0)
+            @foreach($category->posts as $post)
+            <div class="card">
+                <div class="card-group">
+                    <div class="row">
+                            <div class="card-body">
+                            <div class="card-title">
+                                <h3>{{ $post->title }}</h3>
+                            </div>
+                            <div class="card-text">
+                                <p>{{ $post->content }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+            @else
+            No post for category {{ $category->name }} found<br>
+            <a href="/posts/create" class="btn btn-success">Add Post</a>
+            @endif
 
 
 </div>
