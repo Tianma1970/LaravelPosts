7f6f17b1a3b20d247253fa42175efc14a6d88098
Tillmann Weimer <tillmann1970@gmail.com>
2020-02-20 17:32:34 +0100

    added bootstrap classes

--- resources/views/motto/create.blade.php
+++ resources/views/motto/create.blade.php
@@ -1,25 +1,33 @@
 @extends('layouts/app')
 
 @section('content')
+<div class="card  col-8">
+                <div class="card-group">
+                    <div class="row">
+                            <div class="card-body">
+                                <div class="card-title">
+                                    <h1>Create your Motto</h1>
+                                </div>
 
-    <form method="POST" action="/motto">
-        @csrf
-    <!--Content-->
+                                    <form method="POST" action="/motto">
+                                    @csrf
+                                    <!--Content-->
 
-            <div class="form-group col-6">
-                <h1>Create your Motto</h1>
-                <label for="content">Content</label>
-                <textarea id="content" name="content"
-                 class="form-control" placeholder="Your Motto">{{ $user->motto }}</textarea>
-            </div>
+                                        <div class="form-group">
+                                            <label for="content">Content</label>
+                                            <textarea id="content" name="content"
+                                            class="form-control" placeholder="Your Motto">{{ $user->motto }}</textarea>
+                                        </div>
 
-    <!--Submit-->
-            <div class="form-group">
-                <input type="submit" value="Create Motto"
-                class="btn btn-success">
-            </div>
-    </form>
+                                    <!--Submit-->
+                                        <div class="form-group">
+                                            <input type="submit" value="Create Motto"
+                                            class="btn btn-success">
+                                        </div>
+                                    </form>
 
-
-
+                            </div>
+                    </div>
+                </div>
+</div>
 @endsection
