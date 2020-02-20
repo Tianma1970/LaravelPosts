dcafca09b31bd35d7eec47f19ac22406d195eaee
Tillmann Weimer <tillmann1970@gmail.com>
2020-02-20 17:20:58 +0100

    replaced motto with location

--- resources/views/locations/create.blade.php
+++ resources/views/locations/create.blade.php
@@ -2,20 +2,20 @@
 
 @section('content')
 
-<form method="POST" action="/motto" class="col-6">
+<form method="POST" action="/location" class="col-6">
         @csrf
     <!--Content-->
 
             <div class="form-group col-6">
-                <h1>Create your Motto</h1>
+                <h1>Add your location</h1>
                 <label for="content">Content</label>
                 <textarea id="content" name="content"
-                 class="form-control" placeholder="Your Motto">{{ $user->motto }}</textarea>
+                 class="form-control" placeholder="Your Location">{{ $user->location }}</textarea>
             </div>
 
     <!--Submit-->
             <div class="form-group">
-                <input type="submit" value="Create Motto"
+                <input type="submit" value="Add Location"
                 class="btn btn-success">
             </div>
     </form>
