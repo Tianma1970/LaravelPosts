e5ee5a323c98e5765e301b94faf74004f335e196
Tillmann Weimer <tillmann1970@gmail.com>
2020-02-20 10:55:21 +0100

    added relationships

--- app/Location.php
+++ app/Location.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Location extends Model
 {
-    //
+    protected $fillable = ['name'];
+
+    public function location() {
+        $this->belongsTo(User::class());
+    }
+
+    public function users() {
+        $this->hasMany(User::class());
+    }
+
 }
