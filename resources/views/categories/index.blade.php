a8a03289a79c945411aad5a8b2635e78362d10e0
Tillmann Weimer <tillmann1970@gmail.com>
2020-02-21 16:56:17 +0100

    added delete one or more categories

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -6,24 +6,30 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">All Categories</div>
-                <div class="card-body">
-                    <div class="jumbotron">
+                    <div class="card-body">
+                        <div class="jumbotron">
                             @include('partials/status')
-                            <ul>
-                            @foreach($categories as $category)
-                                    <li>
-                                    <a href="/categories/{{ $category->id }}">{{ $category->name }}</a>
-                                    <input type="checkbox" name="category[]" value="{{ $category->id }}">
-                            @endforeach
-                                    </li>
-                                </ul>
-                                <a href="/categories/create" class="btn btn-secondary">Add a new category</a>
-                                <a href="/categories/" class="btn btn-danger">Delete selected category</a>
-                            </div>
+                            <form method="POST" action="/categories/delete">
+                            @csrf
+
+                                @foreach($categories as $category)
+                                        <div>
+
+                                            <input type="checkbox" name="ids[]" value="{{ $category->id }}">
+                                            <a href="/categories/{{ $category->id }}">{{ $category->name }}</a>
+                                        </div>
+                                @endforeach
+
+                                    <a href="/categories/create" class="btn btn-secondary">Add a new category</a>
+
+                                    <input class="btn btn-danger" type="submit" value="Delete selected category" />
+
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-   </div>
+    </div>
+</div>
 @endsection
