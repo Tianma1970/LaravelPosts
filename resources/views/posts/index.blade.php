c48ebc4e7f99c3e8e60f25d456da8d082d3bbf29
Tillmann Weimer <tillmann1970@gmail.com>
2020-02-24 13:54:06 +0100

    moved status message

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts/app')
 
 @section('content')
-@include('partials/status')
 
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">All Posts</div>
-                    <div class="card-body">
-                        <div class="jumbotron">
+                <div class="card-body">
+                    @include('partials/status')
+                    <div class="jumbotron">
                             <ul>
                                 @foreach($posts as $post)
                                 <li><a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
