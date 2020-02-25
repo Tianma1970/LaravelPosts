b4b5d44bde00517a4f5c919444ca4abab8176426
Tillmann Weimer <tillmann1970@gmail.com>
2020-02-25 17:25:09 +0100

    moved status link

--- resources/views/Allposts/index.blade.php
+++ resources/views/Allposts/index.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts/app')
 
 @section('content')
-@include('partials/status')
 
 
 <div class="container">
     <div class="card col-8">
         <h1 class="text-center mt-3">All Posts</h1>
+        @include('partials/status')
         <div class="container mt-3">
             <div class="jumbotron col-12">
                 <ul>
