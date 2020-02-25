1f79dc86cbd4eda2de3d1d9b975d3574d2e7d293
Tillmann Weimer <tillmann1970@gmail.com>
2020-02-25 17:25:52 +0100

    added selct to form

--- resources/views/comments/create.blade.php
+++ resources/views/comments/create.blade.php
@@ -14,6 +14,39 @@
             <!--Content-->
 
             <div class="form-group">
+                <label for="post_id">Post</label>
+                <select id="post_id" name="post_id"
+                class="form-control">
+                <option value="">Select the post you want to comment</option>
+                @foreach($posts as $post)
+                    <option value="{{ $post->id }}"
+                        @if($post->id == old('post_id'))
+                            selected
+                            @endif
+                            >
+                                {{ $post->title }}
+                        </option>
+                @endforeach
+
+
+            </select>
+            <div class="form-group">
+                <label for="author">Your Name</label>
+                <input type="author" id="author"
+                  name="author" value="{{ old('author') }}"
+                  placeholder="Your Name"
+                  class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label for="email">Your Email</label>
+                <input type="email" id="email"
+                  name="email" value="{{ old('email') }}"
+                  placeholder="Your E-mail"
+                  class="form-control">
+            </div>
+
+
                 <label for="content">Content</label>
                 <textarea id="content" name="content"
                  class="form-control" placeholder="Your Comment">{{ old('content') }}</textarea>
