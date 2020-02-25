b0812393d2d4638080a5e4294a2b460cf4d87ec1
Tillmann Weimer <tillmann1970@gmail.com>
2020-02-25 13:35:04 +0100

    added bootstrap classes

--- resources/views/Allposts/index.blade.php
+++ resources/views/Allposts/index.blade.php
@@ -3,16 +3,20 @@
 @section('content')
 @include('partials/status')
 
-<div class="container mt-3">
-    <h1>All Posts</h1>
 
-    <ul>
-        @foreach($post as $post)
-            <li><a href="/Allposts/{{ $post->id }}">{{ $post->title }}</a>
-            </li>
-
-        @endforeach
-    </ul>
+<div class="container">
+    <div class="card col-8">
+        <h1 class="text-center mt-3">All Posts</h1>
+        <div class="container mt-3">
+            <div class="jumbotron col-12">
+                <ul>
+            @foreach($post as $post)
+                    <li><a href="/Allposts/{{ $post->id }}">{{ $post->title }}</a>
+                </li>
+            @endforeach
+                </ul>
+            </div>
+        </div>
+    </div>
 </div>
-
 @endsection
--- resources/views/comments/index.blade.php
+++ resources/views/comments/index.blade.php
@@ -1,13 +1,20 @@
 @extends('layouts/app')
 
 @section('content')
+<div class="container">
+<div class="card col-8">
+        <h1 class="text-center mt-3">All comments</h1>
+        <div class="container mt-3">
+            <div class="jumbotron col-12">
+                <ul>
+            @foreach($comments as $comment)
+                    <li>{{ $comment->content }}</li>
 
-<ul>
-@foreach($comments as $comment)
-    <li>{{ $comment->content }}</li>
-
-@endforeach
-</ul>
-
+            @endforeach
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
