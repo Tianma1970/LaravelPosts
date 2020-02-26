c3140058cecf14214be2a8030702a74675eeca38
Tillmann Weimer <tillmann1970@gmail.com>
2020-02-26 11:41:08 +0100

    added select category to form

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -10,16 +10,33 @@
             @csrf
             @method('PUT')
             @include('partials/validation_errors')
-                    <div class="form-group">
-                        <label for="title">Title</label>
-                        <input type="text" id="title"
-                          name="title" required value="{{ old('title') ? old('title') : $post->title }}">
-                    </div>
-                    <!--Content-->
-                    <div class="form-group">
-                        <label for="content">Content</label>
-                        <input type="text" name="content" class="form-control"  required value="{{ old('content') ? old('content') : $post->content }}">
-                    </div>
+
+            <!--Category-->
+
+            <div class="form-group">
+                <label for="category_id">Category</label>
+                <select id="category_id" name="category_id" class="form-control">
+                     <option value="">Please select a category</option>
+                     @foreach($categories as $category)
+                        <option value="{{ $category->id }}"
+                            @if($category->id == old('category_id'))
+                            selected
+                            @endif
+                            >
+                                {{ $category->name }}
+                        </option>
+                     @endforeach
+                 </select>
+            </div>
+            <div class="form-group">
+                <label for="title">Title</label>
+                <input type="text" id="title"name="title" required value="{{ old('title') ? old('title') : $post->title }}">
+            </div>
+            <!--Content-->
+            <div class="form-group">
+                <label for="content">Content</label>
+                <input type="text" name="content" class="form-control"  required value="{{ old('content') ? old('content') : $post->content }}">
+            </div>
                     <!--Submit-->
             <div class="form-group">
                 <input type="submit" value="Save"
