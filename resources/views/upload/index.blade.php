6896bfc123856fd144dcbacf9b3695c469cb5ba3
Tillmann Weimer <tillmann1970@gmail.com>
2020-02-28 16:27:20 +0100

    added bootstrap classes

--- resources/views/upload/index.blade.php
+++ resources/views/upload/index.blade.php
@@ -1,19 +1,31 @@
-<!doctype html>
-<html>
-  <head>
-    <title>How to Upload a File in Laravel</title>
-  </head>
-  <body>
-    <!-- Message -->
-    @if(Session::has('message'))
-      <p >{{ Session::get('message') }}</p>
-    @endif
+@extends('layouts/app')
 
-    <!-- Form -->
-    <form method='post' action='/uploadFile' enctype='multipart/form-data' >
-      {{ csrf_field() }}
-      <input type='file' name='file' >
-      <input type='submit' name='submit' value='Upload File'>
-    </form>
-  </body>
-</html>
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Upload a file</div>
+                    <div class="card-body">
+                        {{-- @include('partials/status') --}}
+                        <div class="jumbotron">
+
+                            <!-- Message -->
+                            @if(Session::has('message'))
+                            <p >{{ Session::get('message') }}</p>
+                            @endif
+
+                            <!-- Form -->
+                            <form method='post' action='/uploadFile' enctype='multipart/form-data' >
+                            {{ csrf_field() }}
+                            <input type='file' name='file' >
+                            <input type='submit' name='submit' value='Upload File'>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
